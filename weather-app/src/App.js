7611c28a1b495e0d5f93af7906853624c7cacfc4
Brandon_J_Allison <you@example.com>
2019-03-23 17:18:06 -0400

    got API to parse data to JSON

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  constructor(){
+    super()
+    this.state = {
+      getWeather: [],
+      isLoading: false   
+     };
+  }
+  componentDidMount(){
+    this.getWeather('http://api.apixu.com/v1/current.json?key=6865a0ce98c4410ca4f205844192303&q=04062')
+  }
+
+  getWeather = URL => {
+
+    fetch(URL)
+    .then(response => {
+      
+      return response.json();
+    }).then(data =>  {
+     
+      console.log(data);  
+    });
+
+  };
+
+  
+
+  render(){
+    return(
+      <div>
+        <h1>Let's Take A Look at That Forecast</h1>
+
       </div>
-    );
+    )
   }
 }
+
+
 
 export default App;
--- weather-app/src/NavBar.js
+++ weather-app/src/NavBar.js
@@ -1,12 +1,16 @@
 import React from 'react'
 const NavBar = () => {
-
+    return (
+    
     <div>
         
 
     </div>
 
-}
+    )
+    
+
+};
 
 
 
